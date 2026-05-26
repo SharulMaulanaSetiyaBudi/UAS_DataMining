cb84c970381548629c5c95efa7a5383ff2760654
Sharul Maulana <24051214219@mhs.unesa.ac.id>
2026-05-26 16:27:00 +0700

    Fix prediction model

--- save_model.py
+++ save_model.py
@@ -3,49 +3,75 @@ import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 
-# Load dataset
+# =========================
+# LOAD DATASET
+# =========================
 df = pd.read_csv(
     "dataset/WA_Fn-UseC_-Telco-Customer-Churn.csv"
 )
 
-# Hapus customerID
-df = df.drop(
-    columns=["customerID"]
-)
+# =========================
+# PILIH FITUR
+# =========================
+df = df[[
+    "gender",
+    "SeniorCitizen",
+    "tenure",
+    "MonthlyCharges",
+    "TotalCharges",
+    "Churn"
+]]
 
-# Bersihkan TotalCharges
-df["TotalCharges"] = (
-    df["TotalCharges"]
-    .replace(" ", "0")
-)
-
+# =========================
+# BERSIHKAN DATA
+# =========================
 df["TotalCharges"] = pd.to_numeric(
-    df["TotalCharges"]
+    df["TotalCharges"],
+    errors="coerce"
 )
 
-# Encoding otomatis SEMUA string
-df = pd.get_dummies(
-    df,
-    drop_first=True
+df["TotalCharges"] = df["TotalCharges"].fillna(
+    df["TotalCharges"].median()
 )
 
-# Pisahkan fitur dan target
+# =========================
+# ENCODING
+# =========================
+df["gender"] = df["gender"].map({
+    "Male": 1,
+    "Female": 0
+})
+
+df["Churn"] = df["Churn"].map({
+    "Yes": 1,
+    "No": 0
+})
+
+# =========================
+# FEATURE & TARGET
+# =========================
 X = df.drop(
-    columns=["Churn_Yes"]
+    "Churn",
+    axis=1
 )
 
-y = df["Churn_Yes"]
+y = df["Churn"]
 
-# Model
+# =========================
+# MODEL
+# =========================
 model = RandomForestClassifier(
-    n_estimators=100,
     random_state=42
 )
 
-# Training
+# =========================
+# TRAINING
+# =========================
 model.fit(X, y)
 
-# Save model
+# =========================
+# SAVE MODEL
+# =========================
 joblib.dump(
     model,
     "model/model.pkl"
